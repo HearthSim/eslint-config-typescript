9f26bf3256849e1b869fabaa13ebd97e99cb94b0
Benedict Etzel <benedict@hearthsim.net>
2021-02-16 15:56:25 +0100

    Add ESLint configuration

--- README.md
+++ README.md
@@ -1,3 +1,55 @@
 # @hearthsim/eslint-config-typescript
 
 This repository contains the HearthSim common ESLint configuration.
+
+## Setup
+
+1. Install
+
+```bash
+$ yarn add -D eslint @hearthsim/eslint-config-typescript
+```
+
+2. Install ESLint plugins  
+These must be installed with the project :
+
+```bash
+$ yarn add -D eslint-plugin-prettier@^4.15.1 \
+              eslint-plugin-react@^7.22.0 \
+              eslint-plugin-react-hooks@^4.2.0 \
+              eslint-plugin-unused-imports@^1.1.0 \
+              eslint-plugin-jest@^24.1.3 \
+              @typescript-eslint/eslint-plugin@^4.15.1
+```
+
+3. Configure ESLint:
+
+Create or update your`.eslintrc.js`:
+```js
+module.exports = {
+	extends: ["@hearthsim/eslint-config-typescript"],
+	parserOptions: {
+		project: "./tsconfig.json",
+	},
+	env: {
+		browser: true,
+	},
+};
+```
+
+4. Add the scripts to your `package.json`:
+
+```json
+{
+	"scripts": {
+		"lint:eslint": "eslint --cache ./"
+	}
+}
+
+```
+
+## Philosophy
+
+### Zero tolerance for Errors false positives
+   If a rule is not able to handle the ways we legitimately use it, it needs to be downgraded to a warning. 
+   Errors are reserved for critical  issues that can directly introduce security risks, break the application or lead to severe performance penalties. 
---
+++ index.js
@@ -0,0 +1,94 @@
+module.exports = {
+	plugins: [
+		"@typescript-eslint",
+		"unused-imports",
+		"jest",
+	],
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:jest/recommended",
+		"plugin:react/recommended",
+		"plugin:react-hooks/recommended",
+		// prettier always need to be last
+		"prettier",
+		"prettier/@typescript-eslint"
+	],
+	settings: {
+		react: {
+			version: "detect"
+		}
+	},
+	overrides: [
+		{
+			files: ["**/*.js", "**/*.jsx"],
+			rules: {
+				// require is allowed in js, as we may not have import available
+				"@typescript-eslint/no-var-requires": "off",
+			}
+		}
+	],
+	rules: {
+		"@typescript-eslint/no-explicit-any": "off",
+		"@typescript-eslint/explicit-function-return-type": "off",
+		"@typescript-eslint/no-empty-interface": "warn",
+		"@typescript-eslint/ban-types": [
+			"warn", {
+				types: {
+					"{}": {
+						"message": "If you're trying to do something like React.FC<{}>, use React.FC instead",
+					},
+				},
+				extendDefaults: true,
+			},
+		],
+		// non-null assertions aren't great, but sometimes a last resort
+		"@typescript-eslint/no-non-null-assertion": "off",
+		// redundant types are totally okay, they can signal developer intent
+		"@typescript-eslint/no-inferrable-types": "off",
+		// these can be acceptable
+		"@typescript-eslint/no-empty-function": "off",
+		// we're not so strict on external module exports and trust typescript's interference here
+		"@typescript-eslint/explicit-module-boundary-types": "off",
+		// replaced by no-unused-imports, which can also handle typescript
+		"@typescript-eslint/no-unused-vars": "off",
+		// no case declarations are not necessarily a bug, however they can lead to misused variables
+		"no-case-declarations": "warn",
+		"no-empty-pattern": "warn",
+		// we're pretty lenient on these, and they can help tighten types
+		"no-extra-boolean-cast": "off",
+		// consts are generally preferred in this case
+		"prefer-const": "warn",
+		// mostly used for auto-fixing
+		"unused-imports/no-unused-imports": "warn",
+		"unused-imports/no-unused-vars": ["warn", {
+			varsIgnorePattern: "^_",
+			argsIgnorePattern: "^_",
+			caughtErrorsIgnorePattern: "^_",
+		}],
+		// we don't use this
+		"react/display-name": "off",
+		// not great, but otherwise we get false positives with <Trans> (localization)
+		"react/jsx-key": "warn",
+		// The following rule is very sensible, enforcing noopener is critical. However, we're usually okay with
+		// exposing the referrer. Unfortunately it can't handle styled-components anchors.
+		"react/jsx-no-target-blank": ["error", {
+			"allowReferrer": true
+		}],
+		// our build chain can handle literals
+		"react/no-unescaped-entities": "off",
+		// doesn't work reliably with typescript (`FC<Props>` vs. `{} :Props`)
+		"react/prop-types": "off",
+		// we shouldn't usually have to worry about this
+		"react/react-in-jsx-scope": "off",
+		// acceptable as long as it's only imported from other tests
+		"jest/no-export": "warn",
+		// the following warning catches errors like
+		//   `expect(button.props().href === "/test");`
+		// and is thus an error
+		"jest/valid-expect": "error",
+		// a bad title with e.g. a space at the end is okay, but rather warn
+		"jest/valid-title": "warn",
+	}
+};
+
